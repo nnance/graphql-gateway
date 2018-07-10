1eeaec0122618aa1f1a46113a61fa67148572352
Nick Nance <nance.nick@gmail.com>
2018-07-10 10:32:48 -0700

    user schema and impl

--- packages/user/src/impl.ts
+++ packages/user/src/impl.ts
@@ -1,13 +1,44 @@
-import {GraphQLObjectType, GraphQLSchema, GraphQLString} from "graphql";
+import {
+    makeExecutableSchema,
+} from "graphql-tools";
 
-export default new GraphQLSchema({
-    query: new GraphQLObjectType({
-        fields: {
-            testString: {
-                resolve: () => "Hello world",
-                type: GraphQLString,
-            },
-        },
-        name: "Query",
-    }),
+interface IUser {
+    _id: number;
+    blogs?: string[];
+    username: string;
+}
+
+const user = (id: number, username: string, blogs?: string[]) => ({
+    _id: id, blogs, username,
 });
+
+const users = [
+    user(1, "test-user-1"),
+    user(2, "test-user-2"),
+    user(3, "test-user-3"),
+];
+
+const userById = (id: number) => {
+    // tslint:disable-next-line:no-console
+    console.log(`queryUser: ${id}`);
+    return users.find((u) => u._id === id);
+};
+
+const typeDefs = `
+    type User {
+        _id: ID!
+        username: String
+        blogs: [String]
+    }
+    type Query {
+        users: [User]
+    }
+`;
+
+const resolvers = {
+    Query: {
+        users: () => users,
+    },
+};
+
+export default makeExecutableSchema({typeDefs, resolvers});
