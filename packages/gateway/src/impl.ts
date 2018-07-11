f494b24af4720cac783b8b0ad90e0682482b8ef1
Nick Nance <nance.nick@gmail.com>
2018-07-10 20:49:15 -0700

    working blog integration

--- packages/gateway/src/impl.ts
+++ packages/gateway/src/impl.ts
@@ -6,13 +6,22 @@ import {
 import {
     introspectSchema,
     makeRemoteExecutableSchema,
+    mergeSchemas,
 } from "graphql-tools";
 
 import { createHttpLink } from "apollo-link-http";
 
-const link = createHttpLink({ uri: "http://localhost:3000/graphql", fetch: require("node-fetch") });
-
-export default async () => {
+async function getSchema(uri: string) {
+    const link = createHttpLink({ uri, fetch: require("node-fetch") });
     const schema = await introspectSchema(link);
     return makeRemoteExecutableSchema({ link, schema });
+}
+
+export default async () => {
+    const userLink = await getSchema("http://localhost:3000/graphql");
+    const blogLink = await getSchema("http://localhost:3001/graphql");
+
+    return mergeSchemas({
+        schemas: [ userLink, blogLink ],
+    });
 };
